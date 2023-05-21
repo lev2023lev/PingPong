f3fe3a97d3239ea6b2eded8378a5d58cb76b1b62
lev2023lev <133559377+lev2023lev@users.noreply.github.com>
2023-05-21 13:32:53 +0700

    Update Game.py

--- Game.py
+++ Game.py
@@ -1,5 +1,6 @@
-# PingPong
+from pygame import*
 class GameSprite(sprite.Sprite):
+
     def __init__(self, player_image, player_x, player_y,  size_x, size_y, player_speed):
         super().__init__()
         self.image = transform.scale(image.load(player_image), (65, 65))
@@ -20,3 +21,54 @@ class Player(GameSprite):
     def fire(self):
         bullet = Bullet('bullet.png', self.rect.centerx, self.rect.top, 15, 20, -15)
         bullets.add(bullet)
+
+back = (200, 255, 255)
+win_width = 600
+win_height = 500
+window = display.set_mode((win_width, win_height))
+window.fill(back)
+
+game = True
+finish = False
+clock = time.Clock()
+FPS = 60
+
+racket1 = Player('racket.png',30,200,4,50,150)
+racket2 = Player('racket.png',520,200,4,50,150)
+ball = GameSprite('tenis_ball.png',200,200,4,50,50)
+
+font.init()
+font = font.Fon(None, 35)
+lose1 = font.render('PLAYER 2 WIN!', True, (180,0,0))
+lose2 = font.render('PLAYER 1 WIN!',True, (180,0,0))
+
+speed_x = 3
+speed_y = 3
+
+while game:
+    for e in event.get():
+        if e.tupe == QUIT:
+            game = False
+    if finish != True:
+        window.fill(back)
+        racket1.update_l()
+        racket2.update_r()
+        ball.rect.x += speed_x
+        ball.rect.y += speed_y
+
+        if sprite.collide_rect(racket1, ball) or sprite.collide_rect(racket2, ball):
+            speed_x *= -1
+            speed_y *= -1
+
+        if ball.rect.y > win_height-50 or ball.rect.y < 0:
+            speed_y *= -1
+            
+        if ball.rect.x <0:
+            finish = True
+            window.blit(lose1, (200,200))
+            game_over = True
+
+        if ball.rect.x > win_width:
+            finish = True
+            window.blit(lose2, (200,200))
+            game_over = True
